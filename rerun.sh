b37555095a26a59531cf5c3d1fdeda48ba6dc4ea
Victor Chen <yvictorck@gmail.com>
2015-11-04 22:55:59 -0800

    inf bench mark

--- rerun.sh
+++ rerun.sh
@@ -7,7 +7,7 @@ date && echo "start uni medium uni moderate" && \
 date && echo "start uni heavy uni moderate" && \
 ./run_bench_Array_granular.sh myapp uni-heavy 10 uni-moderate && \
 date && echo "start bimo-medium uni long" && \
-./run_bench_Array_granular.sh myapp bimo-medium 10 uni-longRTXen && \
+./run_bench_Array_granular_bimo_moderate.sh myapp bimo-medium 10 uni-moderate && \
 date && echo "starting ratio" && \
 ./run_ratio_fig2_else.sh myapp && date
 date && echo "starting ratio" && \
