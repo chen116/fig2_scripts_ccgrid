e8b2df8ffa178dbd7eb8ffecb15b7ec0236f57cc
Victor Chen <chen116@usc.edu>
2015-11-23 08:00:20 -0800

    Update run_bench_Array_granular_bimo_moderate.sh

--- run_bench_Array_granular_bimo_moderate.sh
+++ run_bench_Array_granular_bimo_moderate.sh
@@ -41,7 +41,8 @@ do
   for util in 1.0 2.0 3.0 4.0
   do
     #for rep in 0
-    for rep in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
+    for rep in 0 1 2
+    # for rep in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
     do
 
 echo "Starting st_trace"
