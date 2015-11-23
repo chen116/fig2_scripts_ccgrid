118eaedf62652208dc53af2090a59e8c24e91e0d
Victor Chen <chen116@usc.edu>
2015-11-23 10:11:50 -0800

    Update rerun.sh

--- rerun.sh
+++ rerun.sh
@@ -6,14 +6,16 @@ date && echo "start uni medium uni moderate" && \
 ./run_bench_Array_granular.sh myapp uni-medium 10 uni-moderate && \
 date && echo "start uni heavy uni moderate" && \
 ./run_bench_Array_granular.sh myapp uni-heavy 10 uni-moderate && \
+date && echo "start uni heavy uni moderate" && \
+./run_bench_Array_granular.sh myapp bimo-medium 10 uni-moderate && \
 
 #./run_bench_Array_granular.sh myapp bimo-medium 10 uni-longRTXen && \
-date && echo "start bimo-medium uni moderate" && \
-./run_bench_Array_granular_bimo_moderate.sh myapp bimo-medium 10 uni-moderate && \
+#date && echo "start bimo-medium uni moderate" && \
+#./run_bench_Array_granular_bimo_moderate.sh myapp bimo-medium 10 uni-moderate && \
 date && echo "starting ratio" && \
-./run_ratio_fig2_else.sh myapp && date
-date && echo "starting ratio" && \
-./run_ratio_fig2_bimo.sh myapp && date && echo "all done"
+./run_ratio_fig2_else.sh myapp && date && echo "all done"
+#date && echo "starting ratio" && \
+#./run_ratio_fig2_bimo.sh myapp && date && echo "all done"
 
 
 #date && echo "start uni light uni moderate" && \
